def336e7d3a9df931de1c3d84df32cb913463a4a
Antony <fenixstar@mail.ru>
2021-02-28 00:00:24 +0300

    111

--- AppCore/BlockChain/BlockChainCore.pas
+++ AppCore/BlockChain/BlockChainCore.pas
@@ -1,15 +1,53 @@
 unit BlockChainCore;
 
 interface
-
+uses
+  Types.Base,
+  Types.Meta,
+  System.SysUtils,
+  System.Hash;
 type
+  TKey = array [0..31] of Byte;
+  TTransactionType = (RegestrationWallet);
+
   TIndexData = record
-    Index: integer;
+    Index: Int64;
     Size: integer;
   end;
 
-  TTransaction = record
+  TWallet = record
+    WalletName: array [0..31] of byte;
+    WalletPasswordHash: array [0..31] of byte;
+  end;
 
+  TTransaction<T: record> = record
+    TransactionType: TTransactionType;
+    TimeStamp: TDateTime;
+    Data: T;
+    Signature: TBytes;
+  end;
+
+  TBlockHeader = record
+    Version: TVersion;
+    PreviosBlockHash: THashSHA2;
+    ThisBlockHash: THashSHA2;
+    TimeStamp: TDateTime;
+  end;
+
+  TBaseBlock<T:record> = record
+    Size: integer;
+    Header: TBlockHeader;
+    Transaction: T;
+    SignedIt: TBytes;
+    Signature: TBytes;
+  end;
+
+  TChainBase<T: record> = record
+  private
+    Blocks: TArray<T>;
+  public
+    constructor Create;
+    destructor Destroy; override;
   end;
 
   TBlockChainCore = class
@@ -17,5 +55,20 @@ type
   end;
 
 implementation
+
+
+{ TChainBase<T> }
+
+constructor TChainBase<T>.Create;
+begin
+  inherited;
+
+end;
+
+destructor TChainBase<T>.Destroy;
+begin
+
+  inherited;
+end;
 
 end.
---
+++ AppCore/Types.Meta.pas
@@ -0,0 +1,8 @@
+unit Types.Meta;
+
+interface
+type
+  TVersion = Array [0..2] of Byte;
+implementation
+
+end.
